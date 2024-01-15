3c0acf7c2699887b78a8bc9369733049d0207a48
shrfrn <shrfrn@gmail.com>
2024-01-15 16:03:20 +0200

    Init repo

---
+++ index.js
@@ -0,0 +1,241 @@
+// In a real React App we will only have this code ONCE
+// our entire app will be rendered inside the App component
+
+// import { App } from './root-cmp.jsx'
+// const elRoot = document.getElementById('root')
+// const root = ReactDOM.createRoot(elRoot)
+// root.render(
+//     <App />
+// )
+
+////////////////////////////////////////////////////
+
+// :React RAW API compared to JSX
+
+const elRoot = document.getElementById('root')
+const root = ReactDOM.createRoot(elRoot)
+
+const elBox = React.createElement('div',
+    { className: 'container', children: 'Hello React!!!' })
+// const elBox = <div className="container">Hello React!!!</div>
+
+root.render(elBox)
+// console.log('elBox:', elBox)
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// some more JSX
+
+// const names = <ol>
+//     <li>Puki</li>
+//     <li>Muki</li>
+// </ol>
+
+// // console.log('names:', names)
+
+// const myLink = <a href="http://example.com" >
+//     <p className="special">Click me please</p>
+// </a>
+
+// root.render(myLink)
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :JSX Must have one parent element
+
+// This is valid
+// const fruits = <ul>
+//     <li>Apple</li>
+//     <li>Orange</li>
+// </ul>
+
+// TODO: add a heading
+
+// root.render(
+//     fruits
+// )
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :Hello Interpolation - embedding JS expressions inside JSX
+
+// const fullName = 'Puki Ma'
+// const greeting = <h1>Hello {fullName}</h1>
+
+// root.render(
+//     greeting
+// // )
+
+// const sum = 2 + 1
+// const container = <section>
+//     <p>
+//         {sum} ~ {Math.PI.toFixed(5)}
+//     </p>
+//     <p>
+//         {fullName + new Date()}
+//         {console.log('render')}
+//     </p>
+// </section>
+
+
+// root.render(
+//     container
+// )
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :Dynamic Attributes!
+
+// const dynClass = isDark ? 'dark' : 'light'
+
+// Add images
+
+// const imgName = (Math.random() > 0.5) ? 'dog' : 'cat'
+// const dynSrc = `./assets/img/${imgName}.jpg`
+
+// const HelloWorld =
+//     <div>
+//         <h2 className={dynClass}>Some Header</h2>
+//         <img src={dynSrc} alt="" />
+//     </div>
+
+// root.render(HelloWorld)
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :Conditional Rendering
+
+// Option 1 -> just using ifs.
+
+// const isDark = true
+// let button
+// if (isDark) {
+//     button = <button>
+//         Go Light
+//     </button>
+// } else {
+//     button = <button>
+//         Go Dark
+//     </button>
+// }
+
+// root.render(button)
+
+
+///////////////////////////////////////////////
+// Option 2 - The Ternary Operator (short if)
+
+// var age = 5
+// const msg =
+//     <h1>
+//         {(age >= 18) ? <span>Old enough {Math.PI}</span> : <span> {'Too young' + age}</span>}
+//     </h1>
+
+// root.render(msg)
+
+///////////////////////////////////////////////
+// Option 3 - Short Circuit - using &&
+
+// const age = 6
+// const isAdult = true
+
+// const tasty =
+//     <ul>
+//         {(age < 20 && isAdult) && <li>Chips{age} </li>}
+//         {!isAdult && <li>Pizza</li>}
+//         {false}
+//         {null}
+//         {true && <li>hi</li>}
+//         <li>Eggplant</li>
+//         {age > 50 && <li>Grappa</li>}
+//     </ul>
+
+// root.render(tasty)
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :Loops and Arrays
+
+// const names = ['Muki', 'Puki', 'Duki', 'Ruki', 'Muki'];
+
+// root.render(
+//     <ul>
+//         {
+//             names.map((name, idx) => <li key={name + idx}>{name}</li>)
+//         }
+//     </ul>
+// )
+
+// const users = [{ id: 'u101', fullName: 'Muki Ja' }, { id: 'u102', fullName: 'Puki Ma' }]
+
+// root.render(
+//     <ul>
+//         {
+//             users.map(user => <li key={user.id}>{user.fullName}</li>)
+//         }
+//     </ul>
+// )
+
+
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+////////////////////////////////////////////////////
+
+// :Event Handling - passing arguments
+
+// const SomeButton = <button onClick={handleClick}>
+//     Click Me!
+// </button>
+
+// const SomeButton = <button onClick={(ev) => handleClick(ev, msg)}>
+//     Click Me!
+// </button>
+
+// const SomeButton = <button onClick={(ev) => alert( 'hihihi')}>
+//     Click Me!
+// </button>
+
+// function handleClick(...args) {
+//     console.log('args:', args)
+// }
+
+// function handleClick(ev, msg) {
+//     console.log('ev:', ev)
+//     console.log('msg', msg)
+// }
+
+// root.render(
+//     SomeButton
+// )
+
+
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+
+// :React Components - function that returns JSX
+
+// function SomeFuncCmp() {
+//     return <h1>Hello React Component</h1>
+// }
+
+// const section = <section className="container">
+//     <SomeFuncCmp />
+//     {false && <SomeFuncCmp />}
+// </section>
+
+// root.render(
+//     section
+// )
---
+++ root-cmp.jsx
@@ -0,0 +1,21 @@
+function Welcome() {
+    return <div>
+        <h1>Hello there! </h1>
+    </div>
+}
+
+export function App() {
+    const user = { fullName: 'Duki', age: 50 }
+    return (
+        <div>
+            <header>
+                <h1>Lets Play {user.fullName}</h1>
+            </header>
+            
+            <main>
+                <Welcome />
+                <Welcome />
+            </main>
+        </div>
+    )
+}
